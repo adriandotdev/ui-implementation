57f6cd2de46f9846c8aa1f7307e5212ad7643f29
Adrian Nads <marcelonads@gmail.com>
2023-07-28 21:43:17 +0800

    ADDED card design

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <main className="px-10">
+      <main className="p-10">
         <div className="pt-5">
           <h1 className="text-3xl font-bold pb-5">Buttons</h1>
           <div className="buttons flex gap-5 flex-wrap">
@@ -47,26 +47,75 @@ function App() {
           </div>
         </div>
 
-        <div className="pt-5">
+        <div className="pt-5 flex flex-col gap-5">
           <h1 className="text-3xl font-bold pb-5">Upload</h1>
 
-          <div className='flex gap-3 bg-gray-950 max-w-3xl rounded-lg'>
-            <section className='avatar-aang'>
-              {/* <img className='rounded-lg' src="/Avatar.jpg" alt="" /> */}
-            </section>
-            <section className='px-3 py-5'>
-              <h1 className='avatar-title text-6xl font-mono font-bold text-orange-100'>Avatar Aang</h1>
-              <p className='text-3xl text-orange-500'>The Last Airbender</p>
-              <span className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat reprehenderit dolore, expedita, fugiat temporibus mollitia at culpa vel, blanditiis veniam asperiores corporis nulla molestiae quam. Lorem ipsum dolor sit amet consectetur adipisicing elit.?</span>
+          <div className="avatar-scroll flex flex-nowrap overflow-x-auto">
 
-              <section className='pt-5 flex gap-2'>
-                <button className='bg-blue-900 px-5 py-2 rounded-md text-white font-medium hover:bg-blue-700 transition-all focus:ring-2 focus:ring-offset-2 outline-none focus:ring-blue-900 focus:bg-blue-700 focus:shadow-lg focus:shadow-blue-500/50 hover:shadow-lg hover:shadow-blue-500/50'>Watch</button>
+            <div className="min-w-full xl:min-w-[50rem] xl:max-w-[50rem] mx-5">
+              <div className='flex flex-col md:flex-row gap-3 bg-gray-950 rounded-lg'>
+                <section className='avatar-kyoshi min-h-[15rem]'>
+                  {/* <img className='rounded-lg' src="/Avatar.jpg" alt="" /> */}
+                </section>
+                <section className='px-5 pt-0 pb-5 md:pt-5'>
+                  <h1 className='avatar-title text-3xl md:text-5xl xl:text-6xl font-mono font-bold text-amber-500'>Avatar Kyoshi</h1>
+                  <p className='text-xl text-orange-200 md:text-2xl xl:text-3xl pb-2'>The Avatar of the Earth Kingdom</p>
+                  <span className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat reprehenderit dolore, expedita, fugiat temporibus mollitia at culpa vel, blanditiis veniam asperiores corporis nulla molestiae quam. Lorem ipsum dolor sit amet consectetur adipisicing elit.?</span>
 
-                <button className='bg-yellow-900 px-5 py-2 rounded-md text-white font-medium hover:bg-yellow-700 transition-all focus:ring-2 focus:ring-offset-2 outline-none focus:ring-yellow-900 focus:bg--700 focus:shadow-lg focus:shadow-yellow-500/50 hover:shadow-lg hover:shadow-yellow-500/50 min-w-[7rem]'>
-                  Add to Watchlist
-                </button>
-              </section>
-            </section>
+                  <section className='pt-5 flex gap-2'>
+                    <button className='bg-blue-900 px-5 py-2 rounded-md text-white font-medium hover:bg-blue-700 transition-all focus:ring-2 focus:ring-offset-2 outline-none focus:ring-blue-900 focus:bg-blue-700 focus:shadow-lg focus:shadow-blue-500/50 hover:shadow-lg hover:shadow-blue-500/50'>Watch</button>
+
+                    <button className='bg-yellow-900 px-5 py-2 rounded-md text-white font-medium hover:bg-yellow-700 transition-all focus:ring-2 focus:ring-offset-2 outline-none focus:ring-yellow-900 focus:bg--700 focus:shadow-lg focus:shadow-yellow-500/50 hover:shadow-lg hover:shadow-yellow-500/50 min-w-[7rem]'>
+                      Add to Watchlist
+                    </button>
+                  </section>
+                </section>
+              </div>
+            </div>
+
+
+            <div className="min-w-full xl:min-w-[50rem] xl:max-w-[50rem] mx-5">
+              <div className='flex flex-col md:flex-row gap-3 bg-gray-950 rounded-lg'>
+                <section className='avatar-roku min-h-[15rem]'>
+                  {/* <img className='rounded-lg' src="/Avatar.jpg" alt="" /> */}
+                </section>
+                <section className='px-5 pt-0 pb-5 md:pt-5'>
+                  <h1 className='avatar-title text-3xl md:text-5xl xl:text-6xl font-mono font-bold text-amber-500'>Avatar Kyoshi</h1>
+                  <p className='text-xl text-orange-200 md:text-2xl xl:text-3xl pb-2'>The Avatar of the Earth Kingdom</p>
+                  <span className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat reprehenderit dolore, expedita, fugiat temporibus mollitia at culpa vel, blanditiis veniam asperiores corporis nulla molestiae quam. Lorem ipsum dolor sit amet consectetur adipisicing elit.?</span>
+
+                  <section className='pt-5 flex gap-2'>
+                    <button className='bg-blue-900 px-5 py-2 rounded-md text-white font-medium hover:bg-blue-700 transition-all focus:ring-2 focus:ring-offset-2 outline-none focus:ring-blue-900 focus:bg-blue-700 focus:shadow-lg focus:shadow-blue-500/50 hover:shadow-lg hover:shadow-blue-500/50'>Watch</button>
+
+                    <button className='bg-yellow-900 px-5 py-2 rounded-md text-white font-medium hover:bg-yellow-700 transition-all focus:ring-2 focus:ring-offset-2 outline-none focus:ring-yellow-900 focus:bg--700 focus:shadow-lg focus:shadow-yellow-500/50 hover:shadow-lg hover:shadow-yellow-500/50 min-w-[7rem]'>
+                      Add to Watchlist
+                    </button>
+                  </section>
+                </section>
+              </div>
+            </div>
+
+            <div className="min-w-full xl:min-w-[50rem] xl:max-w-[50rem] mx-5">
+              <div className='flex flex-col md:flex-row gap-3 bg-gray-950 rounded-lg'>
+                <section className='avatar-aang min-h-[15rem]'>
+                  {/* <img className='rounded-lg' src="/Avatar.jpg" alt="" /> */}
+                </section>
+                <section className='px-5 pt-0 pb-5 md:pt-5'>
+                  <h1 className='avatar-title text-3xl md:text-5xl xl:text-6xl font-mono font-bold text-amber-500'>Avatar Kyoshi</h1>
+                  <p className='text-xl text-orange-200 md:text-2xl xl:text-3xl pb-2'>The Avatar of the Earth Kingdom</p>
+                  <span className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat reprehenderit dolore, expedita, fugiat temporibus mollitia at culpa vel, blanditiis veniam asperiores corporis nulla molestiae quam. Lorem ipsum dolor sit amet consectetur adipisicing elit.?</span>
+
+                  <section className='pt-5 flex gap-2'>
+                    <button className='bg-blue-900 px-5 py-2 rounded-md text-white font-medium hover:bg-blue-700 transition-all focus:ring-2 focus:ring-offset-2 outline-none focus:ring-blue-900 focus:bg-blue-700 focus:shadow-lg focus:shadow-blue-500/50 hover:shadow-lg hover:shadow-blue-500/50'>Watch</button>
+
+                    <button className='bg-yellow-900 px-5 py-2 rounded-md text-white font-medium hover:bg-yellow-700 transition-all focus:ring-2 focus:ring-offset-2 outline-none focus:ring-yellow-900 focus:bg--700 focus:shadow-lg focus:shadow-yellow-500/50 hover:shadow-lg hover:shadow-yellow-500/50 min-w-[7rem]'>
+                      Add to Watchlist
+                    </button>
+                  </section>
+                </section>
+              </div>
+            </div>
+
           </div>
         </div>
       </main>
